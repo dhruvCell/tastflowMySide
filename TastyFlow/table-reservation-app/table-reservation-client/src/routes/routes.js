79a8a6c0d2d2bc55100d6cf5d065d4ea83dc4a72
dhruvCell <dhruv.sheth@egreycell.com>
2025-09-26 12:53:35 +0530

    make routes file

---
+++ TastyFlow/table-reservation-app/table-reservation-client/src/routes/routes.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import UserPanel from '../components/UserPanel/UserPanel';
+import Login from '../components/LoginSignup/Login';
+import OAuthCallback from '../components/LoginSignup/OAuthCallback';
+import CompleteProfile from '../components/LoginSignup/CompleteProfile';
+import Signup from '../components/LoginSignup/signup';
+import ForgotPassword from '../components/ForgotPassword/ForgotPassword';
+import ResetPassword from '../components/ResetPassword/ResetPassword';
+import Admin from '../components/Sidebar/Admin';
+import Add from '../components/Add/Add';
+import List from '../components/List/List';
+import TableComponent from '../components/TableComponent/TableComponent';
+import UserData from '../components/UserData/UserData';
+import Reviews from '../components/Reviews/Reviews';
+import UserReviews from '../components/UserReviews/UserReviews';
+import About from '../components/About/About';
+import AdminTable from '../components/AdminTable/AdminTable';
+import SlotTable from '../components/TableShow/SlotTable';
+import UsersList from '../components/UsersList/UsersList';
+import UserFoodPage from '../components/UserFoodPage/UserFoodPage';
+import Invoices from '../components/Invoice/Invoice';
+import UserDashBoard from '../components/UserDashboard/UserDashboard';
+import InvoiceList from '../components/InvoiceList/InvoiceList';
+import InvoiceDetail from '../components/UserInvoiceList/UserInvoiceList';
+import EditInvoice from '../components/EditInvoice/EditInvoice';
+import UserInvoice from "../components/UserInvoice/UserInvoice";
+import UserInvoices from "../components/UserInvoices/UserInvoices";
+import UserInvoiceDetail from "../components/UserInvoiceDetail/UserInvoiceDetail";
+import UserMessages from "../components/UserMessages/UserMessages";
+import Graph from "../components/Graph/Graph";
+import FoodDetail from "../components/FoodDetail/FoodDetail";
+import Services from '../components/Services/Services';
+import BlogDeatils from '../components/BlogDetails/BlogDetails';
+import Recipes from "../components/Recipes/Recipes"
+import Menu_Page from '../components/Menu Page/Menu_Page';
+import Info from '../components/Info/Info';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_CLIENT_KEY);
+
+function PrivateRoute({ element, ...rest }) {
+  const token = localStorage.getItem("token");
+  return token ? element : <Navigate to="/login" />;
+}
+
+function AdminRoute({ element, ...rest }) {
+  const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+  return userDetails?.role === 'admin' ? element : <Navigate to="/" />;
+}
+
+function AppRoutes({ showAlert }) {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login showAlert={showAlert} />} />
+      <Route path="/auth/callback" element={<OAuthCallback showAlert={showAlert} />} />
+      <Route path="/complete-profile" element={<PrivateRoute element={<CompleteProfile showAlert={showAlert} />} />} />
+      <Route path="/" element={<UserPanel showAlert={showAlert} />} />
+      <Route path="/signup" element={<Signup showAlert={showAlert} />} />
+      <Route path="/info" element={<PrivateRoute element={<Info showAlert={showAlert} />} />} />
+      <Route path="/forgot-password" element={<ForgotPassword showAlert={showAlert} />} />
+      <Route path="/reset-password" element={<ResetPassword showAlert={showAlert} />} />
+      <Route path="/table-reserve" element={<PrivateRoute element={<Elements stripe={stripePromise}><TableComponent showAlert={showAlert} /></Elements>} />}/>
+      <Route path="/admin" element={<AdminRoute element={<Admin showAlert={showAlert} />} />} />
+      <Route path="/admin/list" element={<AdminRoute element={<List showAlert={showAlert} />} />} />
+      <Route path="/admin/add" element={<AdminRoute element={<Add showAlert={showAlert} />} />} />
+      <Route path="/admin/all-users" element={<AdminRoute element={<UserData showAlert={showAlert} />} />} />
+      <Route path="/admin/all-reviews" element={<AdminRoute element={<Reviews showAlert={showAlert} />} />} />
+      <Route path="/admin/users/reviews/:userId" element={<PrivateRoute element={<UserReviews />} />} />
+      <Route path="/admin/admin-table" element={<AdminRoute element={<AdminTable showAlert={showAlert} />} />} />
+      <Route path="/admin/slot/:slotNumber" element={<AdminRoute element={<SlotTable showAlert={showAlert} />} />} />
+      <Route path="/admin/create-bill" element={<AdminRoute element={<UsersList showAlert={showAlert} />} />} />
+      <Route path="/admin/user/:userId/create-bill" element={<AdminRoute element={<UserFoodPage showAlert={showAlert} />} />} />
+      <Route path="/admin/invoice" element={<AdminRoute element={<Invoices showAlert={showAlert} />} />} />
+      <Route path="/admin/user/dash-board/:userId" element={<AdminRoute element={<UserDashBoard showAlert={showAlert} />} />} />
+      <Route path="/admin/all-invoices" element={<AdminRoute element={<InvoiceList showAlert={showAlert} />} />} />
+      <Route path="/admin/invoices/:invoiceId" element={<AdminRoute element={<InvoiceDetail showAlert={showAlert} />} />} />
+      <Route path="/admin/invoices/edit/:invoiceId" element={<AdminRoute element={<EditInvoice showAlert={showAlert} />} />} />
+      <Route path="/admin/users/invoice/:userId" element={<PrivateRoute element={<UserInvoice />} />} />
+      <Route path="/user/invoices" element={<PrivateRoute element={<UserInvoices />} />} />
+      <Route path="/user/messages" element={<PrivateRoute element={<UserMessages />} />} />
+      <Route path="/user/:invoiceId" element={<PrivateRoute element={<UserInvoiceDetail />} />} />
+      <Route path="/admin/graph" element={<AdminRoute element={<Graph />} />} />
+      <Route path="/food/:id" element={<FoodDetail />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Menu_Page" element={<Menu_Page />} />
+      <Route path="/Recipes" element={<Recipes />} />
+      <Route path='/Services' element={<Services/>}/>
+      <Route path='/BlogDetails' element={<BlogDeatils/>}/>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
